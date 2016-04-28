6983780537a7f50ad8e1df45bb59673140a354e5
Aaron Corso <aaron@corsoa.com>
2016-04-28 20:25:09 +0100

    Install Oh-My-Zsh and syntax highlighting on new machines.

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 shopt -s dotglob
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for f in $DIR/*; do
