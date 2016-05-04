0c7cb25a9abd184a1aaa719effe1b76a5bf3af88
root <root@localhost.localdomain>
2016-05-04 17:27:28 -0400

    Add a few setup steps to set dotfiles (Vundle, zsh, tmux)

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+if command_exists yum ; then
+    yum install zsh tmux
+elif command_exists apt-get ; then
+    apt-get install zsh tmux
+fi
 sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 shopt -s dotglob
@@ -9,3 +17,8 @@ for f in $DIR/*; do
         ln -fs $DIR/$(basename $f) ~/$(basename $f)
     fi
 done
+ln -fs /usr/bin/vim /usr/bin/vi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
+#reload tmux conf in case we're already in a tmux session
+tmux source-file ~/.tmux.conf
