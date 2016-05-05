0ac54315f8af9a55e49545a46d52628b44545b08
Aaron Corso <aaron@corsoa.com>
2016-05-05 19:51:09 +0000

    Install basic command line tools

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -9,6 +9,18 @@ elif command_exists apt-get ; then
 fi
 sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#command line tools
+if !command_exists pip ; then
+    wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
+    sudo python get-pip.py
+fi
+if !command_exists fpp ; then
+    git clone https://github.com/facebook/pathpicker.git
+    cd PathPicker
+    ln -fs "$(pwd)/fpp" /usr/local/bin/fpp
+fi
+
 shopt -s dotglob
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for f in $DIR/*; do
