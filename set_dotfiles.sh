0b443c6d64917f9f40670748fe37a3ae5fa7a7ae
Aaron Corso <aaron@corsoa.com>
2016-06-12 15:00:44 -0400

    Improved initialization script.

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -2,20 +2,32 @@
 command_exists () {
     type "$1" &> /dev/null ;
 }
-if command_exists yum ; then
-    yum install zsh tmux
-elif command_exists apt-get ; then
-    apt-get install zsh tmux
+
+#install and change shell to zsh if not already
+if ! command_exists zsh
+then
+    if command_exists yum ; then
+        sudo yum install zsh tmux
+    elif command_exists apt-get ; then
+        sudo apt-get install zsh tmux
+    fi
 fi
-sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "ZSH NOT INSTALLED"
+    sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then 
+    echo "SYNTAX NOT INSTALLED"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
 
 #command line tools
-if !command_exists pip ; then
+if ! command_exists pip; then
     wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
     sudo python get-pip.py
 fi
-if !command_exists fpp ; then
+if ! command_exists fpp ; then
     git clone https://github.com/facebook/pathpicker.git
     cd PathPicker
     ln -fs "$(pwd)/fpp" /usr/local/bin/fpp
@@ -29,8 +41,19 @@ for f in $DIR/*; do
         ln -fs $DIR/$(basename $f) ~/$(basename $f)
     fi
 done
-ln -fs /usr/bin/vim /usr/bin/vi
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall
+
+#do git diffs with vim
+sudo ln -fs $DIR/git_diff_wrapper /usr/bin/git_diff_wrapper 
+
+#reassign vi to vim
+sudo ln -fs /usr/bin/vim /usr/bin/vi
+
+#do a vundle install
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall
+
 #reload tmux conf in case we're already in a tmux session
 tmux source-file ~/.tmux.conf
