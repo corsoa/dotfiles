63d60eca2ad90e0cf46187d0dffd5dea33efb05e
Aaron Corso <aaron@corsoa.com>
2016-09-11 15:10:34 -0400

    Install git on machines that don't have it to satisfy dependencies.

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -3,15 +3,20 @@ command_exists () {
     type "$1" &> /dev/null ;
 }
 
-#install and change shell to zsh if not already
-if ! command_exists zsh
-then
-    if command_exists yum ; then
-        sudo yum install zsh tmux
-    elif command_exists apt-get ; then
-        sudo apt-get install zsh tmux
-    fi
-fi
+install_if_not_exists () {
+   if ! command_exists "$1"; then
+       if command_exists yum ; then
+           sudo yum install "$1"
+       elif command_exists apt-get ; then
+           sudo apt-get install "$1"
+       fi
+   fi
+}
+
+install_if_not_exists git
+install_if_not_exists zsh
+install_if_not_exists tmux
+
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "ZSH NOT INSTALLED"
     sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
