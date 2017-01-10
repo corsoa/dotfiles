b566326bf6d7709a2b1f6d08715dac1782189265
Aaron Corso <aaron@corsoa.com>
2017-01-09 23:02:18 -0500

    Improve dotfiles setup

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -6,24 +6,37 @@ command_exists () {
 install_if_not_exists () {
    if ! command_exists "$1"; then
        if command_exists yum ; then
-           sudo yum install "$1"
+           sudo yum -y install "$1"
        elif command_exists apt-get ; then
-           sudo apt-get install "$1"
+           sudo apt-get -y install "$1"
        fi
    fi
 }
 
+set_context () {
+    #cd into the same directory where this script is located first so sym links are always correct
+    shopt -s dotglob
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    cd $DIR
+    for f in $DIR/*; do
+        if [ $(basename $f) != ".git" && $(basename $f) == ".*" ]
+        then
+            ln -fs $DIR/$(basename $f) ~/$(basename $f)
+        fi
+    done
+}
+
+set_context
 install_if_not_exists git
 install_if_not_exists zsh
 install_if_not_exists tmux
+install_if_not_exists vim
 
 if [ ! -d ~/.oh-my-zsh ]; then
-    echo "ZSH NOT INSTALLED"
     sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
 if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then 
-    echo "SYNTAX NOT INSTALLED"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
@@ -33,19 +46,12 @@ if ! command_exists pip; then
     sudo python get-pip.py
 fi
 if ! command_exists fpp ; then
-    git clone https://github.com/facebook/pathpicker.git
-    cd pathpicker
-    ln -fs "$(pwd)/fpp" /usr/local/bin/fpp
-fi
-
-shopt -s dotglob
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for f in $DIR/*; do
-    if [ $(basename $f) != ".git" ] 
-    then
-        ln -fs $DIR/$(basename $f) ~/$(basename $f)
+    if [ ! -d ~/.pathpicker ]; then
+        git clone https://github.com/facebook/pathpicker.git ~/.pathpicker
+        ln -fs ~/.pathpicker/fpp /usr/local/bin/fpp
+        cd ../
     fi
-done
+fi
 
 #do git diffs with vim
 #sudo ln -fs $DIR/git_diff_wrapper /usr/bin/git_diff_wrapper 
@@ -61,5 +67,9 @@ fi
 vim +PluginInstall +qall
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
+
+source ~/.zshrc
 #reload tmux conf in case we're already in a tmux session
-tmux source-file ~/.tmux.conf
+if [ -n "$TMUX" ]; then
+    tmux source-file ~/.tmux.conf
+fi
